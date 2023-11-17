0df27e80205b1fcc5e93dee1bc2f416bf7173577
Jake <that.seuya@gmail.com>
2023-11-17 15:54:34 -0800

    update: cart checkout button

--- assets/jtzuya.js
+++ assets/jtzuya.js
@@ -3259,6 +3259,50 @@ __webpack_require__.r(__webpack_exports__);
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"];
 alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start();
 
+// if (document.querySelector('[data-cart-drawer]') !== undefined || document.querySelector('[data-cart-drawer]') !== null) document.querySelector('[data-cart-drawer]').addEventListener('click', observeCart)
+
+// function observeCart() {
+//     let cartState = JSON.parse(document.querySelector('[data-cart-drawer]').getAttribute('data-card-drawer'))
+
+//     switch(cartState) {
+//         case true:
+//             document.body.style.overflow = 'hidden'
+//             break
+//         case false:
+//             document.body.style.overflow = 'unset'
+//             break
+//         default:
+//             console.error('something is wrong with the cartState value.')
+//             break
+//     }
+// }
+
+// function cartHandler(e) {
+//     const cartDrawer = document.querySelectorAll('[data-cart-drawer]')
+
+//     cartDrawer.forEach(i => {
+//         let cartState = JSON.parse(i.getAttribute('data-cart-drawer'))
+
+//         switch(cartState) {
+//             case true:
+//                 document.body.style.overflow = 'hidden'
+//                 i.setAttribute('data-cart-drawer', false)
+//                 i.classList.add('-right-full')
+//                 i.classList.remove('right-0')
+//                 break
+//             case false:
+//                 document.body.style.overflow = 'unset'
+//                 i.setAttribute('data-cart-drawer', true)
+//                 i.classList.remove('-right-full')
+//                 i.classList.add('right-0')
+//                 break
+//             default:
+//                 console.error('something is wrong with the cartState value.')
+//                 break
+//         }
+//     })
+// }
+
 /***/ }),
 
 /***/ "./src/scss/jtzuya.scss":
--- src/js/jtzuya.js
+++ src/js/jtzuya.js
@@ -2,3 +2,47 @@ import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 Alpine.start()
+
+// if (document.querySelector('[data-cart-drawer]') !== undefined || document.querySelector('[data-cart-drawer]') !== null) document.querySelector('[data-cart-drawer]').addEventListener('click', observeCart)
+
+// function observeCart() {
+//     let cartState = JSON.parse(document.querySelector('[data-cart-drawer]').getAttribute('data-card-drawer'))
+
+//     switch(cartState) {
+//         case true:
+//             document.body.style.overflow = 'hidden'
+//             break
+//         case false:
+//             document.body.style.overflow = 'unset'
+//             break
+//         default:
+//             console.error('something is wrong with the cartState value.')
+//             break
+//     }
+// }
+
+// function cartHandler(e) {
+//     const cartDrawer = document.querySelectorAll('[data-cart-drawer]')
+
+//     cartDrawer.forEach(i => {
+//         let cartState = JSON.parse(i.getAttribute('data-cart-drawer'))
+        
+//         switch(cartState) {
+//             case true:
+//                 document.body.style.overflow = 'hidden'
+//                 i.setAttribute('data-cart-drawer', false)
+//                 i.classList.add('-right-full')
+//                 i.classList.remove('right-0')
+//                 break
+//             case false:
+//                 document.body.style.overflow = 'unset'
+//                 i.setAttribute('data-cart-drawer', true)
+//                 i.classList.remove('-right-full')
+//                 i.classList.add('right-0')
+//                 break
+//             default:
+//                 console.error('something is wrong with the cartState value.')
+//                 break
+//         }
+//     })
+// }
